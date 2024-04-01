34b8a9f4df331b0cb4a01d50925fbce61fffe25d
Shubham Damai <damaishubham14@gmail.com>
2024-04-02 01:09:41 +0530

    Initial commit: Adding existing Django project

---
+++ userauths/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+class user(AbstractUser):
+    email = models.EmailField(unique = True)
+    username = models.CharField(max_length=100)
+    
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ['username']
+    
+    def __str__(self):
+        return self.username
