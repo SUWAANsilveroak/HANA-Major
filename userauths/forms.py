fd8ca6ae5e7797cbd72e6dc4b8fa52f9cc577a25
Shubham Damai <damaishubham14@gmail.com>
2024-04-04 01:45:32 +0530

    registration pending

--- userauths/forms.py
+++ userauths/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from userauths.models import user 
 
-class UserRegisterForm(forms.ModelForm):
+class UserRegisterForm(UserCreationForm):
     
     class Meta:
         model = user
--- userauths/views.py
+++ userauths/views.py
@@ -1,10 +1,24 @@
 from django.shortcuts import render
 from userauths.forms import UserRegisterForm
+from django.contrib.auth import login, authenticate 
+from django.contrib import messages
 
 # Create your views here.
 def register_view(request):
-    form = UserRegisterForm()
     
+    if request.method=="POST":
+        form = UserRegisterForm(request.POST or None)
+        if form.is_valid():
+            new_user = form.save()
+            username = form.cleaned_data.get("username")
+            messages.success(request, f"Hey{username}, your account was created successfully")
+            new_user = authenticate(username=form.cleaned_data["email"],
+                                    password =form.cleaned_data["password1"])
+            login(request, new_user)
+            return ("core:index")
+    else:
+        form = UserRegisterForm()
+        
     context={
         'form': form,
     }
