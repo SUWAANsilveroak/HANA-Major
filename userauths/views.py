036a09eb9649b1127e8aa54d94460e98cbfe3bbe
Shubham Damai <damaishubham14@gmail.com>
2024-04-05 00:25:41 +0530

    sign-up form complited

--- userauths/views.py
+++ userauths/views.py
@@ -1,25 +1,27 @@
-from django.shortcuts import render
-from userauths.forms import UserRegisterForm
-from django.contrib.auth import login, authenticate 
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
 from django.contrib import messages
+from userauths.forms import UserRegisterForm
 
-# Create your views here.
 def register_view(request):
-    
-    if request.method=="POST":
-        form = UserRegisterForm(request.POST or None)
+    if request.method == "POST":
+        form = UserRegisterForm(request.POST)
         if form.is_valid():
             new_user = form.save()
-            username = form.cleaned_data.get("username")
-            messages.success(request, f"Hey{username}, your account was created successfully")
-            new_user = authenticate(username=form.cleaned_data["email"],
-                                    password =form.cleaned_data["password1"])
-            login(request, new_user)
-            return ("core:index")
+            email = form.cleaned_data.get("email")
+            password = form.cleaned_data.get("password1")
+            # Authenticate the new user
+            new_user = authenticate(username=email, password=password)
+            if new_user:
+                # Login the user
+                login(request, new_user)
+                messages.success(request, f"Hey {new_user.username}, your account was created successfully")
+                # Redirect to the desired URL after successful login
+                return redirect("core:index")
+            else:
+                messages.error(request, "Failed to authenticate. Please try logging in.")
     else:
         form = UserRegisterForm()
-        
-    context={
-        'form': form,
-    }
-    return render(request,"userauths/sign-up.html", context)
+
+    context = {'form': form}
+    return render(request, "userauths/sign-up.html", context)
